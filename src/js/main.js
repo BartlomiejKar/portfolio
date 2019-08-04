cacca599e6acc7053706116ba3b3c93d8faa1edd
Bartek Karelus <bartek.karelus@gmail.com>
2019-08-04 20:09:11 +0200

    add styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -19,4 +19,15 @@ if ('serviceWorker' in navigator) {
 
 console.log(`Hello world!`);
 
+// const list = document.querySelector(".project__div")
+// fetch("https://api.github.com/users/BartlomiejKar/repos?sort=created")
+//   .then(response => response.json())
+//   .then(data => {
+//     const repos = data;
+//     for (const item of repos) {
+//       list.innerHTML += (`${item.name} ${item.html_url}`)
+//       console.log(item.name)
+//     }
+
+//   })
 
