b1de9fcb82e15d4b51c1c030e0002c3b259f939e
Bartek Karelus <bartek.karelus@gmail.com>
2019-08-08 20:43:47 +0200

    fixed validator html

--- src/js/main.js
+++ src/js/main.js
@@ -29,14 +29,14 @@ fetch("https://api.github.com/users/BartlomiejKar/repos?sort=created")
       <section class="section__projects">
       <div class="project__div">
         <img src="assets/img/github.png" alt="github" class="github">
-        <span class="project__div--title">${item.name}</span>
+        <span class="project__div-title">${item.name}</span>
         <p class="project__description">${item.description ? item.description : "brak opisu"}</p>
       </div>
       <footer class="project__footer">
       <img src="assets/img/Demo Icon.svg" alt ="demo" class="demo">
-    <a class="project__footer--demo" href="${item.homepage}" target="_blank" rel="nofollow noreferrer">Demo</a>
+    <a class="project__footer-demo" href="${item.homepage}" target="_blank" rel="nofollow noreferrer">Demo</a>
     <img src = "assets/img/Code Icon.svg" alt = "code" class="code">
-    <a class="project__footer--github" href="${item.html_url}" target="_blank" rel="nofollow noreferrer">Github</a>
+    <a class="project__footer-github" href="${item.html_url}" target="_blank" rel="nofollow noreferrer">Github</a>
       </footer>
 </section > `
       console.log(item.name)
